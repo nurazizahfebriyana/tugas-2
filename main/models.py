e2b421337bbc813e78c594599c4889fd5372a696
nurazizahfebriyana <azizahfebriyana@gmail.com>
2023-09-25 14:15:54 +0700

    Tugas 4, belum README

--- main/models.py
+++ main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
-    nama = models.CharField(max_length=255, default='')
-    kelas = models.CharField(max_length=255, default = '')
     name = models.CharField(max_length=255, default = '')
     price = models.CharField(max_length=255, default = '')
     description = models.TextField()
     amount = models.IntegerField(default=0)
     date_added = models.DateField(auto_now = True, null= True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
